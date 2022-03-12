ade8a1c9e12fb13e54c560d1646517ef921cbb3a
Vitaliy Ponomarenko <glitch.coder.256@gmail.com>
2022-03-12 18:53:58 +0200

    C1. team lead добавил класс C1
    В классе C1 созданны функции:
    display_1(), display_2()
    
    На ветке master
    
    Начальный коммит
    
    Изменения, которые будут включены в коммит:
    	новый файл:    index.php
    	новый файл:    lib/C1.php

---
+++ lib/C1.php
@@ -0,0 +1,14 @@
+<?php
+
+class C1
+{
+    public function display_1()
+    {
+        echo 'C1->display_1';
+    }
+
+    public function display_2()
+    {
+        echo 'C1->display_2';
+    }
+}
